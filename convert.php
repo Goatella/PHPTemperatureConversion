c49b58b28ba008d00b44f39cdfdd45bd7347c0a0
MoonAndFox <rachael@moonandfox.com>
2014-03-11 21:11:58 -0700

    Add checks that input exists and is valid.

--- convert.php
+++ convert.php
@@ -1,4 +1,4 @@
- <head> 
+<head> 
  <title>Convert Temperature</title> 
  </head> 
  <body> 
@@ -12,48 +12,64 @@
  </form> 
 
 <?php 
- 
-$scale = $_GET['scale'];
-$degree = $_GET['degree'];
 
+ //check for input
+ if (array_key_exists('degree',$_GET)){
+	$scale = $_GET['scale'];
+	$degree = $_GET['degree'];
+	$firstLength = strlen($_GET['degree']);
+	
+	//check that input is not NULL or NaN
+	if(($firstLength > 0) && (is_numeric($_GET['degree'])))
+		{
+		if ($scale == "celcius") 
+		{
+			 print "<table><tr><th> Conversion Results</th></tr><tr><td>$degree</td><td>celsius</td></tr>"; 
+			 $c_2_f = $degree*9/5+32; 
+			 print "<tr><td>$c_2_f</td><td>fahrenheit</td></tr>"; 
+			 $c_2_k = $degree+273.15; 
+			 print "<tr><td>$c_2_k </td><td>kelvin</td></tr>"; 
+			 $c_2_r = $c_2_f+459.6; 
+			 print "<tr><td>$c_2_r</td><td>rankine</td></tr></table>";} 
 
- if ($scale == "celcius") 
- {print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>celsius</td></tr>"; 
- $c_2_f = $degree*9/5+32; 
- print "<tr><td>$c_2_f</td><td>fahrenheit</td></tr>"; 
- $c_2_k = $degree+273.15; 
- print "<tr><td>$c_2_k </td><td>kelvin</td></tr>"; 
- $c_2_r = $c_2_f+459.6; 
- print "<tr><td>$c_2_r</td><td>rankine</td></tr></table>";} 
-
- if ($scale == "fahrenheit") 
- {print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>farhenheit</td></tr>"; 
- $f_2_c = ($degree -32)*5/9; 
- print "<tr><td>$f_2_c</td><td>celsius</td></tr>"; 
- $f_2_k = $f_2_c+273.15; 
- print "<tr><td>$f_2_k </td><td>kelvin</td></tr>"; 
- $f_2_r = $degree+459.6; 
- print "<tr><td>$f_2_r</td><td>rankine</td></tr></table>";} 
- 
- if ($scale == "kelvin") 
- {print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td> 
- $degree</td><td>kelvin</td></tr>"; 
- $k_2_f = ($degree - 273.15) * 9 / 5 + 32; 
- print "<tr><td>$k_2_f</td><td>fahrenheit</td></tr>"; 
- $k_2_c = $degree-273.15; 
- print "<tr><td>$k_2_c </td><td>celsius</td></tr>"; 
- $k_2_r = $k_2_f+459.6; 
- print "<tr><td>$k_2_r</td><td>rankine</td></tr></table>";} 
- 
- if ($scale == "rankine") 
- {print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>rankine</td></tr>"; 
- $r_2_f = $degree-459.6; 
- print "<tr><td>$r_2_f</td><td>fahrenheit</td></tr>"; 
- $r_2_c = ($r_2_f - 32)*5/9; 
- print "<tr><td>$r_2_c </td><td>celsius</td></tr>"; 
- $r_2_k = $r_2_c + 273.15; 
- print "<tr><td>$r_2_k</td><td>kelvin</td></tr></table>";} 
-
+		if ($scale == "fahrenheit") 
+		{
+			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>farhenheit</td></tr>"; 
+			 $f_2_c = ($degree -32)*5/9; 
+			 print "<tr><td>$f_2_c</td><td>celsius</td></tr>"; 
+			 $f_2_k = $f_2_c+273.15; 
+			 print "<tr><td>$f_2_k </td><td>kelvin</td></tr>"; 
+			 $f_2_r = $degree+459.6; 
+			 print "<tr><td>$f_2_r</td><td>rankine</td></tr></table>";
+		} 
+		 
+		 if ($scale == "kelvin") 
+		{
+			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td> 
+			 $degree</td><td>kelvin</td></tr>"; 
+			 $k_2_f = ($degree - 273.15) * 9 / 5 + 32; 
+			 print "<tr><td>$k_2_f</td><td>fahrenheit</td></tr>"; 
+			 $k_2_c = $degree-273.15; 
+			 print "<tr><td>$k_2_c </td><td>celsius</td></tr>"; 
+			 $k_2_r = $k_2_f+459.6; 
+			 print "<tr><td>$k_2_r</td><td>rankine</td></tr></table>";
+		} 
+		 
+		 if ($scale == "rankine") 
+		{
+			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>rankine</td></tr>"; 
+			 $r_2_f = $degree-459.6; 
+			 print "<tr><td>$r_2_f</td><td>fahrenheit</td></tr>"; 
+			 $r_2_c = ($r_2_f - 32)*5/9; 
+			 print "<tr><td>$r_2_c </td><td>celsius</td></tr>"; 
+			 $r_2_k = $r_2_c + 273.15; 
+			 print "<tr><td>$r_2_k</td><td>kelvin</td></tr></table>";
+		} 
+	 }
+	 else
+		//print an error message if input is NULL or NaN
+		echo "<span style = \"color:red\">*Please Enter a Valid Temperature.</span>";
+}
 
  ?> 
 
